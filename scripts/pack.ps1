0f298b46b9d323b288a52f5d922382376f26ce3c
BMTLab <neverovnikita.bmt@gmail.com>
2021-02-01 18:25:46 +0300

    [F] Add pack script

--- scripts/pack.ps1
+++ scripts/pack.ps1
@@ -4,14 +4,9 @@ $projects = @(
     "..\src\SerialPortListener"
 )
 
-New-Item -ItemType Directory -Force -Path $output
-
-& ./clean.ps1
-
 $projects | ForEach-Object {
     $project = ("{0}\{1}" -f $_,$output)
     $name = $_.Split('\')[2]
     
-    dotnet publish $_ -c Release -o $project
     Compress-Archive -Path $project\* -DestinationPath ("{0}\{1}.zip" -f $project,$name) -Update
 }
